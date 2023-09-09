9539bbeb8ce9334dba86a061937d2b4b3857bfae
Ferdaws Osman Rashid <FDSRashid@users.noreply.huggingface.co>
2023-09-09 18:05:53 +0000

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,18 @@ import gradio as gr
 ar2bw = CharMapper.builtin_mapper('ar2bw')
 bw2ar = CharMapper.builtin_mapper('bw2ar')
 
-def getArabic(transliteration = '*hbt <lY Almktbp.'):
+title = "Transliterate using BuckWater Scheme"
+description = """
+<p>
+<center>
+This app converts transliterated arabic text to Arabic using BuckWater Scheme. Use the following image to familiarize yourself with the conversion system.
+<img src="https://huggingface.co/spaces/FDSRashid/Camel_tools_Test/blob/main/Buckwalter-transliteration-for-Arabic-language.jpg" alt="Test" width="200"/>
+</center>
+</p>
+"""
+example = [["*hbt <lY Almktbp."]]
+
+def getArabic(transliteration):
   return bw2ar(transliteration)
 
 with gr.Blocks() as demo:
@@ -12,5 +23,6 @@ with gr.Blocks() as demo:
     output = gr.Textbox(label="Arabic Form of Text")
     greet_btn = gr.Button("Get Arabic")
     greet_btn.click(fn=getArabic, inputs=name, outputs=output)
+    examples =  gr.Examples(examples = example, inputs = [name])
 
 demo.launch()
