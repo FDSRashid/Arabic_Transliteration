bfb596cbd85cc5d6e2c0845f52afe13517b19ca2
Ferdaws Osman Rashid <FDSRashid@users.noreply.huggingface.co>
2023-09-09 17:32:18 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 from camel_tools.utils.charmap import CharMapper
 
 import gradio as gr
+ar2bw = CharMapper.builtin_mapper('ar2bw')
+bw2ar = CharMapper.builtin_mapper('bw2ar')
 
-def greet(name):
-    return "Hello " + name + "!"
+def getArabic(transliteration = '*hbt <lY Almktbp.'):
+  return bw2ar(transliteration)
 
-demo = gr.Interface(fn=greet, inputs="text", outputs="text")
-    
-demo.launch()  
+with gr.Blocks() as demo:
+    name = gr.Textbox(label="English Transliteration (Using Buckwater System )")
+    output = gr.Textbox(label="Arabic Form of Text")
+    greet_btn = gr.Button("Get Arabic")
+    greet_btn.click(fn=getArabic, inputs=name, outputs=output)
+
+demo.launch()
